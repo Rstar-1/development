034699c0b60d4856122c5f2a25449a7f85f6452a
Rstar-1 <rajshetye.5855@gmail.com>
2025-01-18 18:06:59 +0530

    d

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-"use client";
-
 import "./styles/globals.scss";
 import "./styles/structure.scss";
 import "./styles/font.scss";
 import { Poppins, Open_Sans } from "next/font/google";
 import Navbar from "./components/Navbar";
 import StoreProvider from "./redux/StoreProvider";
-import Sidebar from "./components/Sidebar";
-import { usePathname } from "next/navigation";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -22,38 +18,12 @@ const openSans = Open_Sans({
 });
 
 export default function HomeLayout({ children }) {
-  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={`${poppins.variable} ${openSans.variable}`}>
         <div>
           <Navbar />
-          <StoreProvider>
-            <div
-              className={
-                pathname === "/barasingha" ? "" : "w-full py12 bg-f4f5f6"
-              }
-            >
-              <div
-                className={
-                  pathname === "/barasingha"
-                    ? ""
-                    : "flex items-start gap-12 px15"
-                }
-              >
-                {pathname === "/barasingha" ? null : <Sidebar />}
-                <div
-                  className={
-                    pathname === "/barasingha"
-                      ? ""
-                      : "w-route cust-scroll overflow-auto"
-                  }
-                >
-                  {children}
-                </div>
-              </div>
-            </div>
-          </StoreProvider>
+          <StoreProvider>{children}</StoreProvider>
         </div>
       </body>
     </html>
